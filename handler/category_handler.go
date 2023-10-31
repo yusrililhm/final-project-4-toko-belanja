233dc0b48486b41b3504e618e95d500a6813e9f1
yusrililhm <yusrililham62@gmail.com>
2023-10-31 14:23:12 +0700

    [category handler] Add: swagger annotaion, body request, path category id and bind json

--- handler/category_handler.go
+++ handler/category_handler.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"strconv"
+	"toko-belanja-app/dto"
+	"toko-belanja-app/pkg/errs"
 	"toko-belanja-app/service/category_service"
 
 	"github.com/gin-gonic/gin"
@@ -22,21 +25,79 @@ func NewCategoryHandler(categoryService category_service.CategoryService) Catego
 }
 
 // AddCategory implements CategoryHandler.
+// AddCategory godoc
+// @Summary Add category
+// @Description Add category
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param Bearer header string true "Bearer Token"
+// @Param dto.CatgoriesRequest body dto.CatgoriesRequest true "body request for add category"
+// @Success 201 {object} dto.CategoryResponse
+// @Router /categories [post]
 func (ch *categoryHandlerImpl) AddCategory(ctx *gin.Context) {
-	
+
+	addRequest := &dto.CategoriesRequest{}
+
+	if err := ctx.ShouldBindJSON(addRequest); err != nil {
+		errBindJson := errs.NewUnprocessableEntityError("invalid json body request")
+		ctx.AbortWithStatusJSON(errBindJson.Status(), errBindJson)
+		return
+	}
 }
 
 // DeleteCategory implements CategoryHandler.
+// DeleteCategory godoc
+// @Summary Delete category
+// @Description Delete category
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param Bearer header string true "Bearer Token"
+// @Param CategoryId path int true "Category Id"
+// @Success 200 {object} dto.CategoryResponse
+// @Router /categories/{categoryId} [delete]
 func (ch *categoryHandlerImpl) DeleteCategory(ctx *gin.Context) {
-	
+	categoryId, _ := strconv.Atoi(ctx.Param("categoryId"))
+	_ = categoryId
 }
 
 // GetCategories implements CategoryHandler.
+// GetCategories godoc
+// @Summary Get categories
+// @Description Get categories
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param Bearer header string true "Bearer Token"
+// @Success 200 {object} dto.CategoryResponse
+// @Router /categories [get]
 func (ch *categoryHandlerImpl) GetCategories(ctx *gin.Context) {
-	
+
 }
 
 // UpdateCategory implements CategoryHandler.
+// UpdateCategory godoc
+// @Summary Update category
+// @Description Update category
+// @Tags Categories
+// @Accept json
+// @Produce json
+// @Param Bearer header string true "Bearer Token"
+// @Param CategoryId path int true "Category Id path"
+// @Param dto.CatgoriesRequest body dto.CatgoriesRequest true "body request for update category"
+// @Success 200 {object} dto.CategoryResponse
+// @Router /categories/{categoryId} [patch]
 func (ch *categoryHandlerImpl) UpdateCategory(ctx *gin.Context) {
-	
+
+	updateRequest := &dto.CategoriesRequest{}
+
+	if err := ctx.ShouldBindJSON(updateRequest); err != nil {
+		errBindJson := errs.NewUnprocessableEntityError("invalid json body request")
+		ctx.AbortWithStatusJSON(errBindJson.Status(), errBindJson)
+		return
+	}
+
+	categoryId, _ := strconv.Atoi(ctx.Param("categoryId"))
+	_ = categoryId
 }
