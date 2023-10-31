f5e6457c8d19a68737f134360720f9c05ad95f4e
yasid <yasidalmubarok@gmail.com>
2023-10-31 22:19:43 +0700

    [User Service] add: Register

--- service/user_service/user_service.go
+++ service/user_service/user_service.go
@@ -1,8 +1,19 @@
 package user_service
 
-import "toko-belanja-app/repository/user_repository"
+import (
+	"net/http"
+	"strconv"
+	"toko-belanja-app/dto"
+	"toko-belanja-app/entity"
+	"toko-belanja-app/pkg/errs"
+	"toko-belanja-app/pkg/helpers"
+	"toko-belanja-app/repository/user_repository"
+)
 
 type UserService interface {
+	RegisterUser(userPayLoad *dto.CreateNewUsersRequest) (*dto.UserResponse, errs.Error)
+	LoginUser(userPayLoad *dto.UsersLoginRequest) (*dto.UserResponse, errs.Error)
+	TopUpBalance(userId int, userPayLoad *dto.UsersTopUpRequest) (*dto.UserResponse, errs.Error)
 }
 
 type userServiceImpl struct {
@@ -12,3 +23,33 @@ type userServiceImpl struct {
 func NewUserService(userRepo user_repository.UserRepository) UserService {
 	return &userServiceImpl{ur: userRepo}
 }
+
+func (us *userServiceImpl) RegisterUser(userPayLoad *dto.CreateNewUsersRequest) (*dto.UserResponse, errs.Error) {
+	err := helpers.ValidateStruct(userPayLoad)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user := &entity.User{
+		FullName: userPayLoad.FullName,
+		Email:    userPayLoad.Email,
+		Password: userPayLoad.Password,
+	}
+
+	user.HashPassword()
+
+	response, err := us.ur.CreateNewUser(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.UserResponse{
+		Code:    http.StatusCreated,
+		Message: "Your account has been successfully created",
+		Data:    response,
+	}, nil
+}
+
+
