f5cf88313a142a6c2702a5806b83674bddfeb28b
yasid <yasidalmubarok@gmail.com>
2023-11-02 06:40:52 +0700

    [Category Service] add: Implements CreateCategory

--- service/category_service/category_service.go
+++ service/category_service/category_service.go
@@ -1,8 +1,11 @@
 package category_service
 
 import (
+	"net/http"
 	"toko-belanja-app/dto"
+	"toko-belanja-app/entity"
 	"toko-belanja-app/pkg/errs"
+	"toko-belanja-app/pkg/helpers"
 	"toko-belanja-app/repository/category_repository"
 )
 
@@ -21,7 +24,29 @@ func NewCategoryService(categoryRepo category_repository.CategoryRepository) Cat
 	return &categoryServiceImpl{cr: categoryRepo}
 }
 
-func (cs *categoryServiceImpl) CreateCategory(categoryPayLoad *dto.CategoriesRequest) (*dto.CategoryResponse, errs.Error)
+func (cs *categoryServiceImpl) CreateCategory(categoryPayLoad *dto.CategoriesRequest) (*dto.CategoryResponse, errs.Error){
+	err := helpers.ValidateStruct(categoryPayLoad)
+
+	if err != nil {
+		return nil, err
+	}
+
+	category := &entity.Category{
+		Type: categoryPayLoad.Type,
+	}
+
+	response, err := cs.cr.CreateCategory(category)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.CategoryResponse{
+		Code: http.StatusCreated,
+		Message: "Category has been successfully created",
+		Data: response,
+	}, nil
+}
 func (cs *categoryServiceImpl) GetAllCategory() (*dto.CategoryResponse, errs.Error)
 func (cs *categoryServiceImpl) UpdateCategory(categoryId int, categoryPayLoad *dto.CategoriesRequest) (*dto.CategoryResponse, errs.Error)
 func (cs *categoryServiceImpl) DeleteCategory(categoryId int) (*dto.CategoryResponse, errs.Error)
