f89fb039f39d63034c951c52d934d99f859d7640
yasid <yasidalmubarok@gmail.com>
2023-11-03 22:40:50 +0700

    [Transaction History Service] fix: Implement CreateTransaction

--- service/transaction_history_service/transaction_history_service.go
+++ service/transaction_history_service/transaction_history_service.go
@@ -1,16 +1,19 @@
 package transaction_history_service
 
 import (
+	"net/http"
 	"toko-belanja-app/dto"
+	"toko-belanja-app/entity"
 	"toko-belanja-app/pkg/errs"
 	"toko-belanja-app/pkg/helpers"
+	"toko-belanja-app/repository/category_repository"
 	"toko-belanja-app/repository/product_repository"
 	"toko-belanja-app/repository/transaction_history_repository"
 	"toko-belanja-app/repository/user_repository"
 )
 
 type TransactionHistoryService interface {
-	CreateTransaction(userId int, transactionPayLoad *dto.TransactionRequest) (*dto.TransactionHistoryResponse, errs.Error)
+	CreateTransaction(productId int, transactionPayLoad *dto.TransactionRequest) (*dto.TransactionHistoryResponse, errs.Error)
 	GetTransactionWithProducts(userId int) (*dto.TransactionHistoryResponse, errs.Error)
 	GetTransactionWithProductsAndUser() (*dto.TransactionHistoryResponse, errs.Error)
 }
@@ -19,6 +22,7 @@ type transactionHistoryServiceImpl struct {
 	thr transaction_history_repository.TransactionHistoryRepository
 	pr  product_repository.ProductRepository
 	ur  user_repository.UserRepository
+	cr	category_repository.CategoryRepository
 }
 
 func NewTransactionHistoryService(transactionHistoryRepo transaction_history_repository.TransactionHistoryRepository, productRepo product_repository.ProductRepository, userRepo user_repository.UserRepository) TransactionHistoryService {
@@ -29,16 +33,61 @@ func NewTransactionHistoryService(transactionHistoryRepo transaction_history_rep
 	}
 }
 
-func(ts *transactionHistoryServiceImpl) CreateTransaction(userId int, transactionPayLoad *dto.TransactionRequest) (*dto.TransactionHistoryResponse, errs.Error) {
+func (ts *transactionHistoryServiceImpl) CreateTransaction(userId int, transactionPayLoad *dto.TransactionRequest) (*dto.TransactionHistoryResponse, errs.Error) {
 	err := helpers.ValidateStruct(transactionPayLoad)
 
 	if err != nil {
 		return nil, err
 	}
 
-	user, err := 
+	product, err := ts.pr.GetProductById(transactionPayLoad.ProductId)
 
+	if err != nil {
+		if err.Status() == http.StatusNotFound {
+			return nil, err
+		}
+		return nil, err
+	}
 
+	if transactionPayLoad.Quantity > product.Stock {
+		return nil, errs.NewUnprocessableEntityError("Insufficient stock for the requested quantity")
+	}
+
+	user, err := ts.ur.GetUserById(userId)
+
+	if err != nil {
+		if err.Status() == http.StatusNotFound {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	totalCost := product.Price * transactionPayLoad.Quantity
+	if user.Balance < totalCost {
+		return nil, errs.NewUnprocessableEntityError("Insufficient balance for the transaction")
+	}
+
+	transaction := &entity.TransactionHistory{
+		UserId: userId,
+		ProductId: transactionPayLoad.ProductId,
+		Quantity: transactionPayLoad.Quantity,
+	}
+
+	response, err := ts.thr.CreateNewTransaction(transaction)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.TransactionHistoryResponse{
+		Code: http.StatusCreated,
+		Message: "You have successfully purchased the product",
+		Data: dto.TransactionBill{
+			TotalPrice: response.TotalPrice,
+			Quantity: response.Quantity,
+			ProductTitle: response.ProductTitle,
+		},
+	}, nil
 }
-func(ts *transactionHistoryServiceImpl) GetTransactionWithProducts(userId int) (*dto.TransactionHistoryResponse, errs.Error)
-func(ts *transactionHistoryServiceImpl) GetTransactionWithProductsAndUser() (*dto.TransactionHistoryResponse, errs.Error)
+func (ts *transactionHistoryServiceImpl) GetTransactionWithProducts(userId int) (*dto.TransactionHistoryResponse, errs.Error)
+func (ts *transactionHistoryServiceImpl) GetTransactionWithProductsAndUser() (*dto.TransactionHistoryResponse, errs.Error)
